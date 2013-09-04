d86dfd1703283b195ba0b96695743b90fca8fa1a
root <root@Kubuntubox.com>
2013-09-03 22:48:15 -0400

    edit

--- AJsChartHelpers.php
+++ AJsChartHelpers.php
@@ -6,6 +6,8 @@
  * Creating a package to manage jquery chart.js
  * function names always start lowercase and are then camel cased
  * variable names are going to be all lowercase
+ *
+ * This is the basest of base classes for the chart helpers.
  * 
  */
 
