aa3ac92492281c59d0c29371942729e4cfff5e58
Kevinwhite1988 <Kevinwhitekw1988@gmail.com>
2013-09-04 23:55:59 -0400

    forgot to add color iterator

---
+++ JsChartColorIterator.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * Author: Kevin White
+ * Date: 9/4/2013
+ * Creating a package to manage jquery chart.js
+ * function names always start lowercase and are then camel cased
+ * variable names are going to be all lowercase
+ *
+ *
+ */
+
+class JsChartColorIterator {
+
+public $colors = array("red","blue","yellow","green","pink","orange","black","white");
+
+}
+
+?>
