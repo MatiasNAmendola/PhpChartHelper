2c2ee60ba819d9b466b13aedc14aa20834d13d8c
Kaydub00 <kevinwhite88@gmail.com>
2013-09-07 01:24:05 -0400

    added basechartdata abstract class for charts js

---
+++ AJsBaseChartData.php
@@ -0,0 +1,76 @@
+<?php
+
+/*
+ * Author: Kevin White
+ * Date: 9/6/2013
+ * Creating a package to manage jquery chart.js
+ * function names always start lowercase and are then camel cased
+ * variable names are going to be all lowercase
+ *
+ *
+ */
+
+
+abstract class AJsBaseChartData extends APhpChartData {
+
+
+protected $values = array();
+protected $colors = array();
+
+public function loadValues(array $values) {
+	foreach($values as $value) {
+		array_push($this->values, $value);
+		$this->counter++;
+	}
+}
+
+public function loadValuesByKey(array $values, $key) {
+	foreach($values as $value) {
+		array_push($this->values, $value[$key]);
+		$this->counter++;
+	}
+}
+
+
+public function loadColors(array $colors) {
+	foreach($colors as $color) {
+		array_push($this->colors, $color[$key]);
+	}
+}
+
+public function loadColorsByKey(array $colors, $key) {
+	foreach($colors as $color) {
+		array_push($this->colors, $color[$key]);
+	}
+}
+
+public function loadArray(array $array, $type = 'values') {
+	if($type == 'values') {
+		$this->loadValues($array);
+	}
+	elseif($type == 'colors') {
+		$this->loadColors($array);
+	}
+}
+
+public function loadKeyArray(array $array, $key, $type = 'values') {
+	if($type == 'values') {
+		$this->loadValuesByKey($array, $key);
+	}
+	elseif($type == 'colors') {
+		$this->loadColorsByKey($array, $key);
+	}
+
+}
+
+public function getValue($int) {
+	return $this->values[$int];
+}
+
+public function getColor($int) {
+	return $this->colors[$int];
+}
+
+}
+
+?>
